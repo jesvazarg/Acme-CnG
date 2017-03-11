57db2b8c88dd3705fe6637770a8b2127c18793cc
sanframar <raznnok@gmail.com>
2017-03-11 18:36:23 +0100

    Creacion de la entidad Message en el paquete de dominio.

---
+++ src/main/java/domain/Message.java
@@ -0,0 +1,106 @@
+
+package domain;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Message extends DomainEntity {
+
+	// Constructors -----------------------------------------------------------
+	public Message() {
+		super();
+	}
+
+
+	// Attributes -------------------------------------------------------------
+	private String				title;
+	private String				text;
+	private Date				sentMoment;
+	private Collection<String>	attachments;
+
+
+	@NotBlank
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(final String title) {
+		this.title = title;
+	}
+
+	@NotBlank
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(final String text) {
+		this.text = text;
+	}
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	public Date getSentMoment() {
+		return this.sentMoment;
+	}
+
+	public void setSentMoment(final Date sentMoment) {
+		this.sentMoment = sentMoment;
+	}
+
+	@NotNull
+	@ElementCollection
+	public Collection<String> getAttachments() {
+		return this.attachments;
+	}
+
+	public void setAttachments(final Collection<String> attachments) {
+		this.attachments = attachments;
+	}
+
+
+	// Relationships ----------------------------------------------------------
+	private Actor	sender;
+	private Actor	recipient;
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Actor getSender() {
+		return this.sender;
+	}
+
+	public void setSender(final Actor sender) {
+		this.sender = sender;
+	}
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	public Actor getRecipient() {
+		return this.recipient;
+	}
+
+	public void setRecipient(final Actor recipient) {
+		this.recipient = recipient;
+	}
+
+}
