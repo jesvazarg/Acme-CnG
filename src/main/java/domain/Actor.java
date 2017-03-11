600fb60d825cc3e8b146d3445ace8a2fb6c57993
Jesus Vazquez Argumedo <Jesus Vazquez Argumedo>
2017-03-11 17:20:51 +0100

    Creacion del dominio y populate de Actor y Administrator

---
+++ src/main/java/domain/Actor.java
@@ -0,0 +1,94 @@
+
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import security.UserAccount;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Actor extends Commentable {
+
+	// Constructors ----------------------------------------------------------
+	public Actor() {
+		super();
+	}
+
+
+	// Attributes -------------------------------------------------------------
+	private String	name;
+	private String	email;
+	private String	phoneNumber;
+
+
+	@NotBlank
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	@NotBlank
+	@Email
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(final String email) {
+		this.email = email;
+	}
+
+	@NotBlank
+	@Pattern(regexp = "(\\+\\d{1,3} )?(\\(\\d{1,3}\\) )?(\\w{4,})")
+	public String getPhoneNumber() {
+		return this.phoneNumber;
+	}
+
+	public void setPhoneNumber(final String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	// Relationships ----------------------------------------------------------
+	private UserAccount			userAccount;
+	private Collection<Comment>	comments;
+
+
+	@NotNull
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL, optional = false)
+	public UserAccount getUserAccount() {
+		return this.userAccount;
+	}
+
+	public void setUserAccount(final UserAccount userAccount) {
+		this.userAccount = userAccount;
+	}
+
+	@NotNull
+	@Valid
+	@OneToMany(mappedBy = "comments")
+	public Collection<Comment> getComments() {
+		return this.comments;
+	}
+
+	public void setComments(final Collection<Comment> comments) {
+		this.comments = comments;
+	}
+
+}
