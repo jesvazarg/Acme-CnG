624f75b8c3b9f8e287228d2b7e438fddce758e43
sanframar <raznnok@gmail.com>
2017-03-13 12:18:29 +0100

    Paquete para la creacion de los test creado y clase de los test de messages creada vacia.

---
+++ src/test/java/services/MessageServiceTest.java
@@ -0,0 +1,25 @@
+
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import utilities.AbstractTest;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class MessageServiceTest extends AbstractTest {
+
+	// System under test ------------------------------------------------------
+
+	@Autowired
+	private MessageService	messageService;
+
+}
